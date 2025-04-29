8741295f1dc660a321dabffd472fcb88bde6a207
James-Pilloff <100033622@mvla.net>
2025-04-28 21:12:38 -0700

    Almost All of Room 1 Art

--- Assets/Scripts/OpenSafe.cs
+++ Assets/Scripts/OpenSafe.cs
@@ -24,6 +24,9 @@ public class OpenSafe : MonoBehaviour
             {
                 GetComponent<InteractiveDetection>().isInteracting = false;
             }
+        } else
+        {
+            safe.GetComponent<SafeBehavior>().open = false;
         }
     }
 }
