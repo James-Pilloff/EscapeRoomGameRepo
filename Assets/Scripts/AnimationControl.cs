0dcd3484fe0e2262fd99a93c90eaa52cac2e0f83
James-Pilloff <100033622@mvla.net>
2025-05-08 14:01:57 -0700

    Animations

--- Assets/Scripts/AnimationControl.cs
+++ Assets/Scripts/AnimationControl.cs
@@ -10,13 +10,16 @@ public class AnimationControl : MonoBehaviour
     private bool isMoving;
     private float xMove;
     private float yMove;
+    private int currentScale;
 
     // Start is called before the first frame update
     void Start()
     {
-        animator.direction = 2;
-        animator.idle = true;
+        animator.SetInteger("direction", 2);
+        animator.SetBool("isIdle", true);
         // 0, 1, 2, 3 correspond to N, E, S, W respectively
+
+        currentScale = 1;
     }
 
     // Update is called once per frame
@@ -28,18 +31,25 @@ public class AnimationControl : MonoBehaviour
         if (xMove < 0)
         {
             animator.SetInteger("direction", 3);
+            transform.localScale = new Vector3(-1, 1, 1);
+            currentScale = -1;
         } else if (xMove > 0)
         {
             animator.SetInteger("direction", 1);
+            transform.localScale = new Vector3(1, 1, 1);
+            currentScale = 1;
         } else if (yMove < 0)
         {
             animator.SetInteger("direction", 2);
+            transform.localScale = new Vector3(1, 1, 1);
+            currentScale = 1;
         } else if (yMove > 0)
         {
             animator.SetInteger("direction", 0);
+            transform.localScale = new Vector3(1, 1, 1);
+            currentScale = 1;
         }
         
         animator.SetBool("isIdle", xMove == 0 && yMove == 0);
-
     }
 }
